b7d74aecb549c7b0bd3029959e845a0b0acb6fb6
Dean Merchant <deanmerchant@gmail.com>
2015-07-09 09:43:44 +0100

    Added decorator component.

---
+++ app/scripts/lib/prepareRoute.js
@@ -0,0 +1,29 @@
+import React, { PropTypes } from 'react';
+
+export default function prepareRoute(prepareFn) {
+
+  return DecoratedComponent =>
+    class PrepareRouteDecorator extends React.Component {
+
+      prepareRoute = prepareFn;
+
+      static contextTypes = {
+        store: PropTypes.object.isRequired
+      };
+
+      render() {
+        return (
+          <DecoratedComponent {...this.props} />
+        );
+      }
+
+      componentDidMount() {
+        const {
+          context: { store },
+          props: { params, location }
+          } = this;
+
+        prepareFn({ store, params, location });
+      }
+    };
+}
