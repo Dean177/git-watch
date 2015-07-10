db5198317803467f62873c0d69c98caec75151b5
Dean Merchant <deanmerchant@gmail.com>
2015-07-10 22:15:27 +0100

    Added functions to check pre-requisites to pulling automatically.

--- README.md
+++ README.md
@@ -6,31 +6,6 @@
  - Download [nwjs](https://github.com/nwjs/nw.js/) and make sure nw is available on your path
  - `npm install`
  
-### git fetch
-https://github.com/nodegit/nodegit/blob/master/examples/fetch.js
-git remote update?
- 
-### Is working directory dirty?
- http://stackoverflow.com/questions/5143795/how-can-i-check-in-a-bash-script-if-my-local-git-repo-has-changes
- `git diff-index --quiet HEAD --;`
-https://github.com/nodegit/nodegit/blob/master/examples/status.js
- 
- ### Are remote/local pointing at different commits & is remote ahead of local?
- (http://stackoverflow.com/questions/3258243/git-check-if-pull-needed)
- git status -uno will tell you whether the branch you are tracking is ahead, behind or has diverged. If it says nothing, 
- the local and remote are the same.
- `
- git status -uno
- On branch master
- Your branch is behind 'origin/master' by 1 commit, and can be fast-forwarded.
-   (use "git pull" to update your local branch)
- 
- nothing to commit (use -u to show untracked files)
- `
-
-### Pull
-(https://github.com/nodegit/nodegit/blob/master/examples/pull.js)
-
 ### Merge clean
 https://github.com/nodegit/nodegit/blob/master/examples/merge-cleanly.js
  
--- app/scripts/lib/PollRepositories.js
+++ app/scripts/lib/PollRepositories.js
@@ -6,26 +6,92 @@ var repoPath = "C:/Users/Dean/workspace/git-watch-test-repo";
 var remoteName = 'origin';
 var remoteMaster = 'master';
 
-
-git.Repository.open(repoPath)
-  .then(function(repository) {
-    return isCurrentBranchRemote(repository, remoteMaster);
-  }).then(function(isCurrentBranchRemote) {
-      console.log("currentBranchIsRemote:", isCurrentBranchRemote);
+pullLatestRemote(repoPath, remoteName, remoteMaster)
+  .then(function() {
+    console.log("repo up to date.");
   })
-  .catch(function(err) { console.error(err); });
+  .catch(function(err) {
+    console.error("error", err);
+  });
 
+function pullLatestRemote(repoPath, remoteName, remoteMaster) {
+  var repository;
+  return git.Repository.open(repoPath)
+    .then(function(repo) {
+      repository = repo;
+      console.log("Fetching...");
+      return fetchAll(repository);
+    })
+    .then(function() {
+      return checkIfWorkingDirectoryIsDirty(repository, remoteMaster, remoteName);
+    })
+    .then(function() {
+      return checkCurrentBranchIsRemote(repository, remoteMaster)
+    })
+    .then(function() {
+      return pullLatest(repository, remoteMaster, remoteName);
+    })
+}
 
+function fetchAll(repository) {
+  return repository.fetch(remoteName, {
+    credentials: function(url, userName) {
+      return git.Cred.sshKeyFromAgent(userName);
+    },
+    certificateCheck: function() {
+      return 1;
+    }
+  });
+}
 
-function isCurrentBranchRemote(repository, remoteBranchName) {
-  return repository.getCurrentBranch()
-    .then(function (ref) {
-      console.log("ref name:", ref.name());
-      var branchName = ref.name().substring('refs/heads/'.length);
-      console.log("branch name:", branchName);
-      return branchName === remoteBranchName;
+function checkIfWorkingDirectoryIsDirty(repository) {
+  return repository.getStatus()
+    .then(function(statuses) {
+      function statusToText(status) {
+        var words = [];
+        if (status.isNew()) { words.push("NEW"); }
+        if (status.isModified()) { words.push("MODIFIED"); }
+        if (status.isTypechange()) { words.push("TYPECHANGE"); }
+        if (status.isRenamed()) { words.push("RENAMED"); }
+        if (status.isIgnored()) { words.push("IGNORED"); }
+
+        return words.join(" ");
+      }
+
+      if (statuses.length !== 0) {
+        var repositoryStatus = statuses.map(function(file) {
+          return file.path() + " " + statusToText(file);
+        });
+
+        console.log("Status: ", repositoryStatus);
+
+        return Promise.reject("Working directory is dirty");
+      } else {
+        return Promise.resolve();
+      }
     });
 }
+
+function checkCurrentBranchIsRemote(repository, remoteBranchName) {
+  return repository.getCurrentBranch().then(function(ref) {
+    var branchName = ref.name().substring('refs/heads/'.length);
+
+    if (branchName != remoteBranchName) {
+      // TODO save the current branch, checkout the branch to track and see if it can be pulled then rebase on it.
+      return Promise.reject("Not currently on the " + remoteMaster + " branch");
+    } else {
+      return Promise.resolve();
+    }});
+}
+
+function pullLatest(repository, branchName, remote) {
+  return repository.mergeBranches(branchName, remote + "/" + branchName).then(function() {
+    return Promise.resolve()
+  }, function() {
+    return Promise.reject("Failed to cleanly merge, local branch is either ahead or diverged");
+  });
+}
+
 
 //const fiveMinutes = 300000; //ms
 //var repositoryPoll = setInterval(() => {
