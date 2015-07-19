47968469f8e25b42d5cf03b7602e8548a6ccb9ef
Dean Merchant <deanmerchant@gmail.com>
2015-07-19 16:54:11 +0100

    Adding redux-dev tools.

--- app/mainApp.jsx
+++ app/mainApp.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import Immutable from 'immutable';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import { Provider } from 'react-redux';
+
+import { devTools, persistState } from 'redux-devtools';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
 import * as reducers from './reducers/';
 import '!style!css!less!./styles/app.less';
 import routes from './routes';
 import Router from 'react-router';
 
+const storeCreator = compose(
+  devTools(),
+  persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/)),
+  createStore
+);
+
 const reducer = combineReducers(reducers);
-const store = createStore(reducer, {});
+const store = storeCreator(reducer);
 const router = Router.create({
   routes: routes,
   location: Router.HashLocation
@@ -17,8 +26,14 @@ const router = Router.create({
 
 router.run(function (Handler) {
   React.render(
-    <Provider {...{ store }} >
-      {() => <Handler /> }
-    </Provider>,
+    <div>
+      <Provider {...{ store }} >
+        {() => <Handler className="app"/> }
+      </Provider>
+      <DebugPanel top right bottom>
+        <DevTools store={store} monitor={LogMonitor} />
+      </DebugPanel>
+    </div>
+    ,
     document.getElementById('react-root'));
 });
